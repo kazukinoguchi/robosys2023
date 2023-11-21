03184475604dc5c2c0698384b9f271a78c9b97ec
Kazuki Noguchi <winniegorst@gmail.com>
2023-11-21 12:20:08 +0900

    chenged a test scrypt for new command

--- test.bash
+++ test.bash
@@ -11,10 +11,10 @@ res=0
 
 ###I/O TEST ###
 out=$(seq 5 | ./plus)
-[ "${out}" = 15 ] || ng ${LINENO}
+[ "${out}" = 14 ] || ng ${LINENO}
 
 out=$(seq 5 | ./multi)
-[ "${out}" = 15 ] || ng ${LINENO}
+[ "${out}" = 120 ] || ng ${LINENO}
 
 [ "$res" = 0 ] && echo OK
 exit $res
